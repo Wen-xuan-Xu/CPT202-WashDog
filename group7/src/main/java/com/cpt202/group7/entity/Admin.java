0398a9ad6fd96c29afe0d3383e3d0133540cca21
N0NEWHY <1270112393@qq.com>
2023-04-04 03:31:24 +0800

    Add User and Customer and Admin Temporarily

---
+++ group7/src/main/java/com/cpt202/group7/entity/Admin.java
@@ -0,0 +1,4 @@
+package com.cpt202.group7.entity;
+
+public class Admin extends User{
+}
---
+++ group7/src/main/java/com/cpt202/group7/entity/User.java
@@ -0,0 +1,21 @@
+package com.cpt202.group7.entity;
+
+import java.util.Date;
+
+public class User {
+    // Primary Key
+    private Integer id;
+
+    // For Login
+    private String username;
+    private String password;
+
+    // User Info
+    private String avatarImgLink;
+    private String nickname;
+    private String gender;
+    private Integer age;
+
+    private String email;
+    private String phone;
+}
