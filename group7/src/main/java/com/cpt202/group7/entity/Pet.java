8458692ad8c8944cb0a62ddac4978ad72d4207c8
N0NEWHY <1270112393@qq.com>
2023-04-04 01:44:44 +0800

    rearrange Pet Class

--- group7/src/main/java/com/cpt202/group7/entity/Pet.java
+++ group7/src/main/java/com/cpt202/group7/entity/Pet.java
@@ -2,8 +2,30 @@ package com.cpt202.group7.entity;
 
 public class Pet {
     private String name;
+    private Integer age;
+    private Double weight;
 
-    public Pet(String name){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
     }
 }
