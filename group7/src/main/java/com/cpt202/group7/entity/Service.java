0181d54f4c6355a3596f967e979f1bec2520d2fd
N0NEWHY <1270112393@qq.com>
2023-04-06 20:29:51 +0800

    Delete The HaseSet Used in Entity Classes

--- group7/src/main/java/com/cpt202/group7/entity/Service.java
+++ group7/src/main/java/com/cpt202/group7/entity/Service.java
@@ -12,10 +12,6 @@ public class Service {
     private String name; // Service Name
     private Double price; // The Price of the Service
 
-    private HashSet<Timestamp> times = new HashSet<>(); // Service Available Time
-    private HashSet<Groomer> groomers = new HashSet<>(); // The Groomers that can offer the service
-    private HashSet<String> petTypes = new HashSet<>(); // What breed of the pet can be served
-
     public Service(String name, Double price) {
         this.name = name;
         this.price = price;
@@ -35,29 +31,5 @@ public class Service {
 
     public void setPrice(Double price) {
         this.price = price;
-    }
-
-    public HashSet<Timestamp> getTimes() {
-        return times;
-    }
-
-    public void setTimes(HashSet<Timestamp> times) {
-        this.times = times;
-    }
-
-    public HashSet<Groomer> getGroomers() {
-        return groomers;
-    }
-
-    public void setGroomers(HashSet<Groomer> groomers) {
-        this.groomers = groomers;
-    }
-
-    public HashSet<String> getPetTypes() {
-        return petTypes;
-    }
-
-    public void setPetTypes(HashSet<String> petTypes) {
-        this.petTypes = petTypes;
     }
 }
