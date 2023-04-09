47bdfd07a527264e7769cd5346b17f337b8d071f
Hongyu Wang <98452186+N0NEWHY@users.noreply.github.com>
2023-04-10 02:45:02 +0800

    Revert "[网络安全版本]"

--- src/main/java/com/cpt202/group7/entity/Customer.java
+++
@@ -1,5 +0,0 @@
-package com.cpt202.group7.entity;
-
-public class Customer extends User{
-
-}
--- src/main/java/com/cpt202/group7/utils/customexceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.cpt202.group7.utils.customexceptions;
-
-public class UserAlreadyExistsException extends Exception {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
-
