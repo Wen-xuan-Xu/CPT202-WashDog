b29aea7fb7d2184bc58f5fe7c460c2faa4b5ec94
songmingping <93943243+songmingping@users.noreply.github.com>
2023-04-10 02:59:22 +0800

    Revert "Revert "[网络安全版本]""

---
+++ src/main/java/com/cpt202/group7/entity/Administrator.java
@@ -0,0 +1,6 @@
+package com.cpt202.group7.entity;
+
+
+public class Administrator extends User{
+
+}
---
+++ src/main/java/com/cpt202/group7/enumerator/PayMethod.java
@@ -0,0 +1,6 @@
+package com.cpt202.group7.enumerator;
+
+public enum PayMethod {
+    Online,
+    Onsite
+}
