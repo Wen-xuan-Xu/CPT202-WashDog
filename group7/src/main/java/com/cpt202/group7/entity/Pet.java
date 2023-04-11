0db895e196500917f4b6952306300a47e8ee4195
N0NEWHY <1270112393@qq.com>
2023-04-11 16:49:46 +0800

    Add Pet

--- group7/src/main/java/com/cpt202/group7/entity/Pet.java
+++ group7/src/main/java/com/cpt202/group7/entity/Pet.java
@@ -8,12 +8,10 @@ public class Pet {
     // Primary Key
     private Integer pet_id;
     private Integer user_id;
-    private String pet_type_id; // The Breed Of the Pet
+    private Integer pet_type_id; // The Breed Of the Pet
     private String sex; // Male or Female
     private Double weight; // > 0
     private Integer age; // 1-50
     private String name; // Length <= 16
     private String tips; // The Note of the pet, max 150 words
-
-
 }
--- group7/src/main/java/com/cpt202/group7/mapper/PetMapper.java
+++ group7/src/main/java/com/cpt202/group7/mapper/PetMapper.java
@@ -3,6 +3,7 @@ package com.cpt202.group7.mapper;
 
 import com.cpt202.group7.entity.Pet;
 import com.cpt202.group7.entity.User;
+import jakarta.servlet.http.PushBuilder;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -15,18 +16,9 @@ public interface PetMapper {
     @Select("select p.name, p.sex, p_type.type, weight FROM pet p JOIN pet_type p_type ON p.pet_type_id = p_type.pet_type_id")
     List<Pet> ShowPet();
 
+    @Select("SELECT pet_type_id FROM pet_type WHERE type = #{type}")
+    public int findPetTypeID(String type);
 
-    @Insert("INSERT INTO Pet(pet_id, name, breed,age,sex,weight,owner, specification) VALUES (#{pet_id}, #{name}, #{breed}, #{age}, #{sex},#{weight},#{owner},#{tips}")
-    int insertpet(@Param("pet_id")Integer pet_id,
-                  @Param("name")String name,
-                  @Param("breed")String breed,
-                  @Param("age")Integer age,
-                  @Param("sex")String sex,
-                  @Param("weight")Double weight,
-                  @Param("owner") User owner,
-                  @Param("tips")String tips);
-
-
-
-
+    @Insert("INSERT INTO Pet(user_id, pet_type_id,sex,weight,age,name, tips) VALUES (#{user_id}, #{pet_type_id}, #{sex}, #{weight},#{weight},#{age},#{name},#{tips}")
+    void insertPet(Pet pet);
 }
