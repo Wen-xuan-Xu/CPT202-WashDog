4477e404102380a59d82202e7c269b728f59147b
LeslieXu <lilmeep727@gmail.com>
2023-04-14 02:25:49 +0800

    [加引用]

--- group7/src/main/java/com/cpt202/group7/mapper/UserMapper.java
+++ group7/src/main/java/com/cpt202/group7/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.cpt202.group7.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cpt202.group7.entity.Pet;
 import com.cpt202.group7.entity.User;
 import org.apache.ibatis.annotations.Insert;
@@ -10,7 +11,7 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-public interface UserMapper extends BaseMapper<User>{
+public interface UserMapper extends BaseMapper<User> {
     @Select("SELECT * FROM user WHERE username = #{username}")
     User findByUsername(String username);
     @Insert("INSERT INTO user(username, password, gender,nickname,phone,role) VALUES(#{username}, #{password}, #{gender}, #{nickname}, #{phone},'CUSTOMER')")
@@ -19,6 +20,9 @@ public interface UserMapper extends BaseMapper<User>{
 
     @Select("SELECT userId FROM user WHERE username = #{username}")
     int getCurrentUserID(String username);
+
+
+
 
 
 }
