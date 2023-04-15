993a01911cc72a83d27e2c9080327c9e1c3a2e5b
songmingping <2533485035@qq.com>
2023-04-15 18:29:01 +0800

    groomer

---
+++ group7/src/main/java/com/cpt202/group7/mapper/GroomerMapper.java
@@ -0,0 +1,27 @@
+package com.cpt202.group7.mapper;
+
+import com.cpt202.group7.entity.Groomer;
+import com.cpt202.group7.entity.Pet;
+import org.apache.ibatis.annotations.*;
+
+import java.awt.*;
+import java.util.List;
+
+
+@Mapper
+public interface GroomerMapper {
+    @Select("select * FROM groomer")
+    List<Groomer> getGroomerList();
+
+    @Select("SELECT * FROM groomer WHERE groomerId = #{groomerId}")
+    Groomer getGroomer(Integer groomerId);
+
+    @Update("UPDATE pet SET name = #{name}, gender = #{gender}, age = #{age}, groomerStarLevelPriceCoefficientId = #{groomerStarLevelPriceCoefficientId}, selfIntroduction = #{selfIntroduction}, workStartTime = #{workStartTime},workEndTime = #{workEndTime} ,isWorking = #{isWorking}WHERE groomerId = #{groomerId}")
+    void updateGroomer(Groomer groomer,Integer groomerId);
+
+    @Insert("INSERT INTO `cpt202-group7`.`groomer` (`name`, `gender`, `age`, `groomerStarLevelPriceCoefficientId`, `selfIntroduction`, `workStartTime`, `workEndTime`,`isWorking`) VALUES (#{name}, #{gender}, #{age}, #{groomerStarLevelPriceCoefficientId}, #{selfIntroduction}, #{workStartTime}, #{workEndTime},#{isWorking})")
+    void insertGroomer(Groomer groomer);
+
+    @Delete("DELETE FROM `cpt202-group7`.`groomer` WHERE `groomerId` = #{groomerId}")
+    void deleteGroomer(Integer groomerId);
+}
