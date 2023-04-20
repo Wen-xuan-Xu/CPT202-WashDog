dde01e355c546f661023a8a974032a34f4ada27a
N0NEWHY <1270112393@qq.com>
2023-04-20 22:26:06 +0800

    Finish all the needs Bugs / Need Solve: 1. Can not show the correct Details about the new appointment that added nearly. 2. Can not calculate the correct total sum for price and duration 3. Backend Only send Star to Frontend, rather than star levels

--- group7/src/main/java/com/cpt202/group7/controller/BookServiceController.java
+++ group7/src/main/java/com/cpt202/group7/controller/BookServiceController.java
@@ -81,12 +81,18 @@ public class BookServiceController {
                                 @RequestParam("time") String time,
 
                                 @RequestParam("services") List<String> services,
-                                @RequestParam("groomers") List<String> groomers
+                                @RequestParam("groomers") List<String> groomers,
+                                @RequestParam("totalPrice") String totalPrice,
+                                @RequestParam("totalDuration") String totalDuration
+
 
     ) {
         System.out.println("Selected Pet Type ID" + petTypeId);
         System.out.println("Selected Date" + date);
         System.out.println("Selected Time" + time);
+
+        System.out.println(totalPrice);
+        System.out.println(totalDuration);
 
         System.out.println("Selected Service Order");
         for (var service : services) {
