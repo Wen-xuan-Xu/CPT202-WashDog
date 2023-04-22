e2b4d1a80919744180b24d16b2fc00a03c11299a
Maimai <99118599+CulMiNate11@users.noreply.github.com>
2023-04-22 15:42:07 +0800

    Update DisplayController.java
    Signed-off-by: Maimai <99118599+CulMiNate11@users.noreply.github.com>

--- group7/src/main/java/com/cpt202/group7/controller/DisplayController.java
+++ group7/src/main/java/com/cpt202/group7/controller/DisplayController.java
@@ -11,16 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/customer/service")
+@RequestMapping("/customer")
 public class DisplayController {
     @Autowired
     private ServiceService serviceService;
+    
+    @Autowired
+    private GroomerService groomerService;
 
-    @RequestMapping("/display")
+    @RequestMapping("/service/display")
     public String displayServices(Model model)
     {
         List<Service> services = serviceService.getServiceList();
         model.addAttribute("service",services);
         return "/service/serviceDisplay";
     }
+        @RequestMapping("/groomer/display")
+    public String displayGroomer(Model model)
+    {
+        List<Groomer> groomers = groomerService.getGroomerList();
+        model.addAttribute("groomer",groomers);
+        return "/groomer/groomerDisplay";
+    }
+    
 }
