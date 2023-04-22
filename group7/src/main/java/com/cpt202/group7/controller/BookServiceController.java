1e92dfb7d1442c4e4dd10b2ee1b3d19c0805d417
N0NEWHY <1270112393@qq.com>
2023-04-23 02:45:20 +0800

    Update Book Services

--- group7/src/main/java/com/cpt202/group7/controller/BookServiceController.java
+++ group7/src/main/java/com/cpt202/group7/controller/BookServiceController.java
@@ -2,11 +2,13 @@ package com.cpt202.group7.controller;
 
 import com.cpt202.group7.entity.*;
 import com.cpt202.group7.service.*;
-import com.cpt202.group7.utils.GenerateOrderUUID.*;
+import com.cpt202.group7.utils.GenerateOrderUUID.UUIDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
@@ -55,6 +57,7 @@ public class BookServiceController {
         model.addAttribute("dates", next14Days);
 
         model.addAttribute("userPhoto", userService.getCurrentUserPhoto());
+        model.addAttribute("userId", userService.getCurrentUserID());
         return "/customer/bookService/base";
     }
 
@@ -75,7 +78,7 @@ public class BookServiceController {
         Timestamp passInStartTime = Timestamp.valueOf(date + " " + time + ":00");
         List<Groomer> groomersList = groomerService.getGroomerListByTheDate(passInStartTime, petTypeId);
         model.addAttribute("groomersList", groomersList);
-        return "/customer/bookService/appointment :: appointmentList";
+        return "/customer/bookService/appointment";
     }
 
     @RequestMapping("/submit")
@@ -144,20 +147,11 @@ public class BookServiceController {
         model.addAttribute("order", order);
         model.addAttribute("services", services1);
         model.addAttribute("groomers", groomers1);
-        model.addAttribute("pet",bookService.getOrderPet(order.getPetId()));
-        model.addAttribute("user",bookService.getOrderUser(order.getUserId()));
-        return "/customer/bookService/confirm";
-    }
+        model.addAttribute("pet", bookService.getOrderPet(order.getPetId()));
+        model.addAttribute("user", bookService.getOrderUser(order.getUserId()));
 
-    @RequestMapping("/confirm")
-    public String getConfirmPage() {
-        return "/customer/bookService/confirm";
+        return "redirect:/customer/" + order.getUserId() + "/orderHistory";
     }
 
 
-    @RequestMapping("/pay")
-    public String successPay(String orderId){
-        bookService.successPay(orderId);
-        return "redirect:/customer/dashboard";
-    }
 }
