1d09059c209580a80ac094e340aa7f0df0a79d01
N0NEWHY <1270112393@qq.com>
2023-04-28 02:04:11 +0800

    1

---
+++ group7/src/main/java/com/cpt202/group7/controller/FileController.java
@@ -0,0 +1,99 @@
+package com.cpt202.group7.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/{role}")
+public class FileController {
+
+    @GetMapping("/upload")
+    public String showUploadPage(@PathVariable("role") String role,
+                                 @RequestParam("category") String category,
+                                 @RequestParam("id") Integer categoryId,
+                                 Model model){
+        model.addAttribute("category", category);
+        model.addAttribute("categoryId",categoryId);
+        System.out.println(role+"---" + category + "---" + categoryId);
+        return "uploadFile";
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(
+            @PathVariable("role") String role,
+            @ModelAttribute("category") String category,
+            @ModelAttribute("categoryId") String categoryId,
+            @RequestParam("file") MultipartFile file,
+            Model model) {
+
+        String url = "/" + role + "/upload?category=" + category + "&id=" + categoryId;
+
+        if (file.isEmpty()) {
+            model.addAttribute("message", "Please select a file to upload.");
+            return "redirect:" + url;
+        }
+
+        try {
+            String[] fileInfo = Objects.requireNonNull(file.getContentType()).split("/");
+            String fileType = fileInfo[0];
+
+            if (!fileType.equals("image")) {
+                model.addAttribute("message", "Failed to upload file: Wrong Type.");
+            } else {
+                String fileName = categoryId + "." + fileInfo[fileInfo.length - 1];
+
+                String resourceDir = System.getProperty("user.dir") + "/src/main/resources/";
+                String uploadDir = resourceDir + "static/img/" + category + "Icon/";
+
+                File dir = new File(uploadDir);
+                if (!dir.exists()) {
+                   boolean success =  dir.mkdirs();
+                }
+                String filePath = uploadDir + fileName;
+                System.out.println(filePath);
+                file.transferTo(new File(filePath));
+
+                model.addAttribute("filename", filePath);
+                model.addAttribute("message", "File uploaded successfully.");
+
+                String imgName = "/img/"+category+"Icon/"+fileName;
+
+                switch (category) {
+                    case "user" -> {
+                        model.addAttribute("u",imgName);
+                        return "";
+                    }
+                    case "groomer" -> {
+                        model.addAttribute("g",imgName);
+                        return "";
+                    }
+                    case "service" -> {
+                        model.addAttribute("s",imgName);
+                        return "";
+                    }
+                    case "petType" -> {
+                        model.addAttribute("p",imgName);
+                        return "";
+                    }
+                    default -> {
+                        model.addAttribute("message", "Failed to upload file");
+                        return "redirect:" + url;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Failed to upload file: " + e.getMessage());
+        }
+        return "redirect:" + url;
+    }
+}
